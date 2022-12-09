f09593964b4be364639a5ee8ea96984dd1bd7341
cheruab <cheruabmelkamu693@gmail.com>
2022-12-09 06:34:39 -0800

    Task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -28,7 +28,8 @@ printf("Last digit of %d is %d and is less than 6 and not 0\n",
 n, n % 10);
 }
 else
-{										printf("Last digit of %d is %d and is 0\n",
+{
+printf("Last digit of %d is %d and is 0\n",
 ni n % 10);
 return (0);
 }
