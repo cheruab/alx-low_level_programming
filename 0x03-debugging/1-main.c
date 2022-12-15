76e0c34dcbb330963142d9d32fd2bf22275260b7
cheruab <cheruabmelkamu693@gmail.com>
2022-12-15 04:05:32 -0800

    Task 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -6,12 +6,18 @@
 int main(void)
 {
 int i;
-printf("Infinite loop incoming :(\n");
+printf("Infinite loop incoming\n")
 i = 0;
+/*
+ * * while (i < 10)
+ * * {
+ * * putchar(i);
+ * * }
+ */
 while (i < 1)
 {
 putchar(i);
 }
-printf("Infinite loop avoided! \\o/\n");
+printf("Infinite loop avoided!\n");
 return (0);
 }
