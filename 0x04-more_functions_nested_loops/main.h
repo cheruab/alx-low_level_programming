8b4896ba852724d419c648b3d2b7d0562b62b435
cheruab <cheruabmelkamu693@gmail.com>
2022-12-19 02:19:44 -0800

    Task 0

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,15 +1,16 @@
-#ifndef _PUTCHAR_H
-#define _PUTCHAR_H
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
 int _putchar(char c);
-int 1_isupper(int c);
+int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
-void print_most_numbers(Void);
+void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
-void print_number(int n)
-#endif /*_PUTCHAR_H */
+void print_number(int n);
+#endif /*MAIN_H */
